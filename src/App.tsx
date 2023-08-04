30fd97255a660a044b6e5c9bf68b666d263910a8
Nikolinc <tsigulskynikita@gmail.com>
2023-08-04 12:56:44 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-center  text-emerald-500 text-[52px] font-bold">
+        Cellular automaton
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/board.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function board() {
+  const cols = 50;
+  const row = 50;
+  
+  return( <div className={`grid grid-cols-${cols} gap-4`}>
+
+  </div>};
+}
+
+export default board;
