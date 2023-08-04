e92bddc48e35ece4c1f8a09b52314fe31a2fef1d
Nikolinc <tsigulskynikita@gmail.com>
2023-08-04 22:18:46 +0500

    connection grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import Grid from "./component/Grid";
+
 function App() {
   return (
     <>
       <div className="text-center  text-emerald-500 text-[52px] font-bold">
         Cellular automaton
+        <div className="mt-12 flex justify-center">
+          <Grid />
+        </div>
       </div>
     </>
   );
