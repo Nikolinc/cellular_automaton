86c4c5173a9d48be835abc4e1e87a962d9a1b669
Nikolinc <tsigulskynikita@gmail.com>
2023-08-05 13:17:33 +0500

    add redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
+import Colors from "./component/Colors";
 import Grid from "./component/Grid";
 
 function App() {
   return (
-    <>
+    <div className="unselectable">
       <div className="text-center  text-emerald-500 text-[52px] font-bold">
         Cellular automaton
-        <div className="mt-12 flex justify-center">
+        <div className="mt-12 flex justify-center gap-5">
           <Grid />
+          <Colors />
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { Provider } from 'react-redux';
 import "./index.css";
+import store from "./store/index.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
