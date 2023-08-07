64dbf4359a539477bffc842caaadb4c281124469
Nikolinc <tsigulskynikita@gmail.com>
2023-08-07 20:35:43 +0500

    add RangerSlider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import React from "react";
 import Colors from "./component/Colors";
 import Grid from "./component/Grid";
+import RangeSlider from "./component/Range";
 
 function App() {
   return (
-    <div className="unselectable">
-      <div className="text-center  text-emerald-500 text-[52px] font-bold">
-        Cellular automaton
-        <div className="mt-12 flex justify-center gap-5">
+    <div className="unselectable text-center  text-emerald-500 text-[52px] font-bold">
+      <div className="mt-2 flex justify-center gap-5">
+        <div className="">
+          <h1 className="mb-5">Cellular automaton</h1>
           <Grid />
+        </div>
+
+        <div
+          className="mt-10
+        "
+        >
+          <RangeSlider />
           <Colors />
         </div>
       </div>
