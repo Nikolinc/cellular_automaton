ef7fc7a8e09679890594c42f219c54e77659e330
Nikolinc <tsigulskynikita@gmail.com>
2023-08-07 10:38:34 +0500

    add calling a modal window

--- src/component/Colors.tsx
+++ src/component/Colors.tsx
@@ -1,36 +1,85 @@
-import { useDispatch } from "react-redux";
+import React from "react";
+import { useRef, useState } from "react";
+import { useSelector } from "react-redux";
 import {
   changeBackground,
   changeCell,
   changeGrid,
+  couterSelector,
 } from "../store/slices/counterSlices";
+import PaletteModal from "./PaletteModal";
 
 function Colors() {
-  const dispatch = useDispatch();
+ 
+  const [openedBackground, setOpenedBackground] = useState(false);
+  const [openedGrid, setOpenedGrid] = useState(false);
+  const [openedCell, setOpenedCell] = useState(false);
+
+  const BackgroundRef = useRef<HTMLButtonElement>(null);
+  const GridRef = useRef<HTMLButtonElement>(null);
+  const CellRef = useRef<HTMLButtonElement>(null);
+
+  const background = useSelector(couterSelector.getBackground);
+  const grid = useSelector(couterSelector.getGrid);
+  const cell = useSelector(couterSelector.getCell);
+
+  const onClose = () => {
+    setOpenedBackground(false);
+    setOpenedGrid(false);
+    setOpenedCell(false);
+  };
 
   return (
     <div className="text-lg grid grid-cols-1 h-48">
       <div className="text-emerald-500 text-[34px] text-left font-bold mb-3">
         COLOR
-      </div>{" "}
-      <button
-        className="w-[232px] h-[34px] bg-neutral-700 rounded-[10px] shadow  hover:border-b-2  border-emerald-500"
-        onClick={() => dispatch(changeBackground("#fff"))}
-      >
-        Background
-      </button>
-      <button
-        className="w-[232px] h-[34px] bg-neutral-700 rounded-[10px] shadow  hover:border-b-2  border-emerald-500"
-        onClick={() => dispatch(changeGrid("#d9d9d9"))}
-      >
-        Border
-      </button>
-      <button
-        className="w-[232px] h-[34px] bg-neutral-700 rounded-[10px] shadow  hover:border-b-2  border-emerald-500"
-        onClick={() => dispatch(changeCell("#fff"))}
-      >
-        Cell
-      </button>
+      </div>
+      <div className="Background">
+        <button
+          className="w-[232px] h-[34px] bg-neutral-700 rounded-[10px] shadow  hover:border-b-2  border-emerald-500"
+          onClick={() => setOpenedBackground((v) => !v)}
+        >
+          Background
+        </button>
+        <PaletteModal
+          opened={openedBackground}
+          triggerRef={BackgroundRef}
+          onClose={onClose}
+          color={background}
+          changeColor={changeBackground}
+        />
+      </div>
+
+      <div className="Grid">
+        <button
+          className="w-[232px] h-[34px] bg-neutral-700 rounded-[10px] shadow  hover:border-b-2  border-emerald-500"
+          onClick={() => setOpenedGrid((v) => !v)}
+        >
+          Grid
+        </button>
+        <PaletteModal
+          opened={openedGrid}
+          triggerRef={GridRef}
+          onClose={onClose}
+          color={grid}
+          changeColor={changeGrid}
+        />
+      </div>
+      <div className="Cell">
+        <button
+          className="w-[232px] h-[34px] bg-neutral-700 rounded-[10px] shadow  hover:border-b-2  border-emerald-500"
+          onClick={() => setOpenedCell((v) => !v)}
+        >
+          Cell
+        </button>
+        <PaletteModal
+          opened={openedCell}
+          triggerRef={CellRef}
+          onClose={onClose}
+          color={cell}
+          changeColor={changeCell}
+        />
+      </div>
     </div>
   );
 }
