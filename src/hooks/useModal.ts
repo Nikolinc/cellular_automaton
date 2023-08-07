6e6d8144be6fa0247b673f9cc3b3bb78d3d8d4b0
Nikolinc <tsigulskynikita@gmail.com>
2023-08-07 10:36:00 +0500

    add hooks for working modal windows

---
+++ src/hooks/useModal.ts
@@ -0,0 +1,50 @@
+
+import { useEffect } from "react";
+import { useEvent } from "./useEvent";
+import { IUseModal } from "../types/Modal";
+
+function useModal({
+  elementRef,
+  triggerRef,
+  enabled = true,
+  onOutsideClick,
+}: IUseModal) {
+  const handleOutsideClick = useEvent(onOutsideClick);
+
+  useEffect(() => {
+    if (!enabled) {
+      return;
+    }
+    const handleClick = (e: MouseEvent | TouchEvent) => {
+      const target = e.target;
+      if (!(target instanceof Node)) {
+        return;
+      }
+
+      if (!elementRef.current) {
+        return;
+      }
+
+      const ignoreElements = [elementRef.current];
+
+      if (triggerRef?.current) {
+        ignoreElements.push(triggerRef.current);
+      }
+
+      if (!ignoreElements.some((element) => element.contains(target))) {
+        console.log("outside click");
+        handleOutsideClick(e);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClick);
+    document.addEventListener("touchstart", handleClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+      document.removeEventListener("touchstart", handleClick);
+    };
+  }, [enabled, elementRef, triggerRef, handleOutsideClick]);
+}
+
+export default useModal;
