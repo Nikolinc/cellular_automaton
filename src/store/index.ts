43912ad50a6fab1964722d79dd1026508c30024b
Nikolinc <tsigulskynikita@gmail.com>
2023-08-07 20:22:11 +0500

    add Size store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterSlices from "./slices/counterSlices";
+import ColorSlices from "./slices/ColorSlices";
+import SizeSlice from "./slices/SizeSlices";
 
 const store = configureStore({
-  reducer: counterSlices.reducer,
+  reducer: {
+    Color: ColorSlices.reducer,
+    Size: SizeSlice.reducer,
+  },
 });
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
