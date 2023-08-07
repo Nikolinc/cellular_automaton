c659c833edaef95b1ef87a63bf164551357f7f4e
Nikolinc <tsigulskynikita@gmail.com>
2023-08-07 10:39:26 +0500

    color change

--- src/component/Cell.tsx
+++ src/component/Cell.tsx
@@ -1,5 +1,5 @@
 // Cell.tsx
-import React from "react";
+import React, { useRef } from "react";
 import { useSelector } from "react-redux";
 import { couterSelector } from "../store/slices/counterSlices";
 
@@ -12,12 +12,17 @@ const Cell: React.FC<CellProps> = ({ alive, onClick }) => {
   const background = useSelector(couterSelector.getBackground);
   const grid = useSelector(couterSelector.getGrid);
   const cell = useSelector(couterSelector.getCell);
+  const tooltipRef = useRef<HTMLDivElement>(null);
 
   return (
     <div
-      className={`cell ${
-        alive ? `bg-[${cell}]` : `bg-[${background}]`
-      } w-5 h-5 border border-[${grid}] `}
+      ref={tooltipRef}
+      style={
+        alive
+          ? { backgroundColor: cell, border: `1px solid ${grid}` }
+          : { backgroundColor: background, border: `1px solid ${grid}` }
+      }
+      className={`cell w-5 h-5 border  `}
       onClick={() => {
         onClick();
       }}
