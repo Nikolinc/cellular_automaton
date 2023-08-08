c365917a05d9498a2528e6e146a03f10e4f55e4d
Nikolinc <tsigulskynikita@gmail.com>
2023-08-08 16:30:57 +0500

    add button stop and pause

--- src/component/ButtonPlay.tsx
+++ src/component/ButtonPlay.tsx
@@ -1,20 +1,81 @@
 import React from "react";
-import { PlaySelector, inversion } from "../store/slices/PlaySlices";
+import {
+  PlaySelector,
+  activatePause,
+  activatePlay,
+  activateStop,
+} from "../store/slices/PlaySlices";
 import { useDispatch, useSelector } from "react-redux";
+import useHover from "../hooks/useHover";
 
 function Play() {
   const dispatch = useDispatch();
   const play = useSelector(PlaySelector.getPlay);
+  const [PlayRef, isPlayHovered] = useHover<HTMLButtonElement>();
+  const [PauseRef, isPauseHovered] = useHover<HTMLButtonElement>();
+  const [StopRef, isStopHovered] = useHover<HTMLButtonElement>();
 
   return (
-    <button
-      className="w-full rounded-[10px] shadow  text-center  bg-neutral-700 text-[34px] font-black text-emerald-500 flex justify-center items-center pb-1 my-8 hover:bg-emerald-500 hover:text-neutral-700"
-      onClick={() => {
-        dispatch(inversion());
-      }}
-    >
-      {play ? `Stop` : `Play`}
-    </button>
+    <>
+      {Boolean(play) ? (
+        <div className="flex justify-center items-center gap-3">
+          <button
+            ref={PauseRef}
+            className=" w-1/3 rounded-[10px] shadow  text-center  bg-neutral-700 text-[34px] font-black text-emerald-500 flex justify-center items-center pb-1.5 my-8 hover:bg-emerald-500 hover:text-neutral-700"
+            onClick={() => {
+              dispatch(activatePause());
+            }}
+          >
+            <svg
+              fill={isPauseHovered ? "#343434" : "#01CC8F"}
+              xmlns="http://www.w3.org/2000/svg"
+              height="48"
+              viewBox="0 -960 960 960"
+              width="48"
+            >
+              <path d="M524-158v-645h279v645H524Zm-367 0v-645h279v645H157Zm459-92h95v-461h-95v461Zm-367 0h95v-461h-95v461Zm0-461v461-461Zm367 0v461-461Z" />
+            </svg>
+          </button>
+          <button
+            ref={StopRef}
+            className=" w-1/3 rounded-[10px] shadow  text-center  bg-neutral-700 text-[34px] font-black text-emerald-500 flex justify-center items-center pb-1.5 my-8 hover:bg-emerald-500 hover:text-neutral-700"
+            onClick={() => {
+              dispatch(activateStop());
+            }}
+          >
+            <svg
+              fill={isStopHovered ? "#343434" : "#01CC8F"}
+              xmlns="http://www.w3.org/2000/svg"
+              height="48"
+              viewBox="0 -960 960 960"
+              width="48"
+            >
+              <path d="M309-651v342-342Zm-91 433v-525h525v525H218Zm91-91h342v-342H309v342Z" />
+            </svg>
+          </button>
+        </div>
+      ) : (
+        <button
+          ref={PlayRef}
+          className=" w-full rounded-[10px] shadow  text-center  bg-neutral-700 text-[34px] font-black text-emerald-500 flex justify-center items-center pb-1.5 my-8 hover:bg-emerald-500 hover:text-neutral-700"
+          onClick={() => {
+            dispatch(activatePlay());
+          }}
+        >
+          Play
+          <svg
+            fill={isPlayHovered ? "#343434" : "#01CC8F"}
+            className="mt-2"
+            xmlns="http://www.w3.org/2000/svg"
+            height="48"
+            viewBox="0 -960 960 960"
+            width="48"
+          >
+            <path d="M298-161v-644l505 322-505 322Zm91-322Zm0 155 245-155-245-155v310Z" />
+          </svg>
+        </button>
+      )}
+    </>
   );
 }
 
