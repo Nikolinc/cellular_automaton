5d74a6695a0ba3fcedf7d2df1c388ff95637fc8b
Nikolinc <tsigulskynikita@gmail.com>
2023-08-08 16:28:59 +0500

    remove dont using link

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ColorSlices from "./slices/ColorSlices";
 import SizeSlice from "./slices/SizeSlices";
-import Play from "../component/ButtonPlay";
 import PlaySlices from "./slices/PlaySlices";
 import RandomSlices from "./slices/DensitySclice";
 
