9a9f6eab6840c50360129da6ef601bd15cfdf4ee
Nikolinc <tsigulskynikita@gmail.com>
2023-08-08 14:35:47 +0500

    fix onClick

--- src/component/ButtonPlay.tsx
+++ src/component/ButtonPlay.tsx
@@ -1,20 +1,16 @@
 import React from "react";
-import { PlaySelector } from "../store/slices/PlaySlices";
+import { PlaySelector, inversion } from "../store/slices/PlaySlices";
 import { useDispatch, useSelector } from "react-redux";
 
 function Play() {
   const dispatch = useDispatch();
   const play = useSelector(PlaySelector.getPlay);
 
-  function changePlay(): any {
-    throw new Error("Function not implemented.");
-  }
-
   return (
     <button
       className="w-full rounded-[10px] shadow  text-center  bg-neutral-700 text-[34px] font-black text-emerald-500 flex justify-center items-center pb-1 my-8 hover:bg-emerald-500 hover:text-neutral-700"
       onClick={() => {
-        dispatch(changePlay());
+        dispatch(inversion());
       }}
     >
       {play ? `Stop` : `Play`}
