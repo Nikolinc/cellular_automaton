4aeded4635210fdb338a1eb063b2a3e038933488
Nikolinc <tsigulskynikita@gmail.com>
2023-08-08 16:28:02 +0500

    add Stop and Pause state

--- src/store/slices/PlaySlices.ts
+++ src/store/slices/PlaySlices.ts
@@ -4,28 +4,42 @@ import { RootState } from "..";
 interface PlayState {
   Play: Boolean;
   Stop: Boolean;
-  Pause:Boolean;
+  Pause: Boolean;
 }
 
 const innitialState: PlayState = {
   Play: false,
   Stop: true,
-  Pause:false
+  Pause: false,
 };
 
 const PlaySlices = createSlice({
   name: "counter",
   initialState: innitialState,
   reducers: {
-    inversion: (state) => {
-      state.Play = !state.Play;
+    activatePlay: (state) => {
+      state.Play = true;
+      state.Pause = false;
+      state.Stop = false;
+    },
+    activateStop: (state) => {
+      state.Play = false;
+      state.Pause = false;
+      state.Stop = true;
+    },
+    activatePause: (state) => {
+      state.Play = false;
+      state.Pause = true;
+      state.Stop = false;
     },
   },
 });
 
 export const PlaySelector = {
   getPlay: (state: RootState) => state.Play.Play,
+  getStop: (state: RootState) => state.Play.Stop,
+  getPause: (state: RootState) => state.Play.Pause,
 };
 
-export const { inversion } = PlaySlices.actions;
+export const { activatePlay, activateStop, activatePause } = PlaySlices.actions;
 export default PlaySlices;
