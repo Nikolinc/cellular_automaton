87d437fe3b0ab22923fde77b9d9ed95f4d03346a
Nikolinc <tsigulskynikita@gmail.com>
2023-08-08 14:30:26 +0500

    add random padding

--- src/component/Grid.tsx
+++ src/component/Grid.tsx
@@ -2,25 +2,45 @@ import React, { useEffect, useState } from "react";
 import Cell from "./Cell";
 import { SizeSelector } from "../store/slices/SizeSlices";
 import { useSelector } from "react-redux";
+import { PlaySelector } from "../store/slices/PlaySlices";
+import { DensitySelector } from "../store/slices/DensitySclice";
+
+const generateRandomGrid = (row: number, col: number, density: number) => {
+  const grid = [];
+  for (let i = 0; i < row; i++) {
+    grid.push(Array.from(Array(col), () => Math.random() < density / 10));
+  }
+  return grid;
+};
+
+const createEmptyGrid = (row: number, col: number) => {
+  const grid = [];
+  for (let i = 0; i < row; i++) {
+    grid.push(Array.from(Array(col), () => false));
+  }
+  return grid;
+};
 
 const Grid: React.FC = () => {
   const numRows = useSelector(SizeSelector.getRows);
   const numCols = useSelector(SizeSelector.getCols);
+  const density = useSelector(DensitySelector.getDensity);
 
+  const play = useSelector(PlaySelector.getPlay);
 
   const [grid, setGrid] = useState<boolean[][]>(
-    Array.from({ length: numRows }, () =>
-      Array.from({ length: numCols }, () => false)
-    )
+    createEmptyGrid(numRows, numCols)
   );
 
   useEffect(() => {
-    setGrid(
-      Array.from({ length: numRows }, () =>
-        Array.from({ length: numCols }, () => false)
-      )
-    );
+    setGrid(createEmptyGrid(numRows, numCols));
   }, [numRows]);
+
+  useEffect(() => {
+    if (play) {
+      setGrid(generateRandomGrid(numRows, numCols, density));
+    }
+  }, [play]);
 
   const toggleCell = (row: number, col: number) => {
     const newGrid = [...grid];
