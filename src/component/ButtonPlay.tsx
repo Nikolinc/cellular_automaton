63122bf0ec3501c5ff0ca93da961309c3ef39f0e
Nikoinc <tsigulskynikita@gmail.com>
2023-08-12 22:44:59 +0500

    fix button play

--- src/component/ButtonPlay.tsx
+++ src/component/ButtonPlay.tsx
@@ -6,26 +6,46 @@ import {
   activateStop,
 } from "../store/slices/PlaySlices";
 import { useDispatch, useSelector } from "react-redux";
-import useHover from "../hooks/useHover";
-
 function Play() {
+
   const dispatch = useDispatch();
   const play = useSelector(PlaySelector.getPlay);
+  const pause = useSelector(PlaySelector.getPause);
   const [firstStart, setfirstStart] = useState<boolean>(true);
-  const [PlayRef, isPlayHovered] = useHover<HTMLButtonElement>();
+
 
   return (
     <div className="flex items-center justify-center">
-      {Boolean(play && !firstStart) ? (
+      {Boolean(!play && firstStart) ?(
+        <button
+          className={" w-3/4 rounded-[10px] shadow  text-center  bg-neutral-700 text-[34px] font-black text-emerald-500 flex justify-center items-center pb-1.5 my-8 hover:bg-emerald-500 hover:text-neutral-700"}
+          onClick={() => {
+            dispatch(activatePlay());
+            setfirstStart(false);
+          }}
+        >
+          Play
+          <svg
+            fill={"#01CC8F"}
+            className="mt-2"
+            xmlns="http://www.w3.org/2000/svg"
+            height="48"
+            viewBox="0 -960 960 960"
+            width="48"
+          >
+            <path d="M298-161v-644l505 322-505 322Zm91-322Zm0 155 245-155-245-155v310Z" />
+          </svg>
+        </button>
+      ):(
         <div className="flex justify-center items-center gap-3">
           <button
-            className=" w-1/3 rounded-[10px] shadow  text-center px-3.5 bg-neutral-700 text-[34px] font-black text-emerald-500 flex justify-center items-center py-0.4  my-8 "
+            className={(play?`bg-emerald-500 text-neutral-700`:` bg-neutral-700 text-emerald-500`) + " w-1/3 rounded-[10px] shadow  text-center px-3.5 text-[34px] font-black text-emerald-500 flex justify-center items-center py-0.4  my-8 hover:bg-emerald-500 hover:text-neutral-700"}
             onClick={() => {
               dispatch(activatePlay());
             }}
           >
             <svg
-              fill={"#01CC8F"}
+              fill={play?"#343434":"#01CC8F"}
               className="mt-2"
               xmlns="http://www.w3.org/2000/svg"
               height="48"
@@ -36,13 +56,13 @@ function Play() {
             </svg>
           </button>
           <button
-            className=" w-1/3 rounded-[10px] shadow  text-center px-3.5 bg-neutral-700 text-[34px] font-black text-emerald-500 flex justify-center items-center py-1  my-8 "
+            className={(pause?`bg-emerald-500 text-neutral-700`:` bg-neutral-700 text-emerald-500`) + " w-1/3 rounded-[10px] shadow  text-center px-3.5 text-[34px] font-black flex justify-center items-center py-1  my-8  hover:bg-emerald-500 hover:text-neutral-700"}
             onClick={() => {
               dispatch(activatePause());
             }}
           >
             <svg
-              fill={"#01CC8F"}
+              fill={pause?"#343434":"#01CC8F"}
               xmlns="http://www.w3.org/2000/svg"
               height="48"
               viewBox="0 -960 960 960"
@@ -52,7 +72,7 @@ function Play() {
             </svg>
           </button>
           <button
-            className=" w-1/3 rounded-[10px] shadow  text-center px-3.5 bg-neutral-700 text-[34px] font-black text-emerald-500 flex justify-center items-center py-1 my-8"
+            className=" w-1/3 rounded-[10px] shadow  text-center px-3.5 bg-neutral-700 text-[34px] font-black text-emerald-500 flex justify-center items-center py-1 my-8  hover:bg-emerald-500 hover:text-neutral-700"
             onClick={() => {
               dispatch(activateStop());
             }}
@@ -68,28 +88,7 @@ function Play() {
             </svg>
           </button>
         </div>
-      ) : (
-        <button
-          ref={PlayRef}
-          className=" w-3/4 rounded-[10px] shadow  text-center  bg-neutral-700 text-[34px] font-black text-emerald-500 flex justify-center items-center pb-1.5 my-8 hover:bg-emerald-500 hover:text-neutral-700"
-          onClick={() => {
-            dispatch(activatePlay());
-            setfirstStart(false);
-          }}
-        >
-          Play
-          <svg
-            fill={isPlayHovered ? "#343434" : "#01CC8F"}
-            className="mt-2"
-            xmlns="http://www.w3.org/2000/svg"
-            height="48"
-            viewBox="0 -960 960 960"
-            width="48"
-          >
-            <path d="M298-161v-644l505 322-505 322Zm91-322Zm0 155 245-155-245-155v310Z" />
-          </svg>
-        </button>
-      )}
+      ) }
     </div>
   );
 }
